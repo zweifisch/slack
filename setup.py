ad1f30fa2df6fd8cc192c39e1421dec18d42af4a
Feng Zhou <zf.pascal@gmail.com>
2014-03-24 21:10:15 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='slack',
+    version='0.0.1',
+    url='https://github.com/zweifisch/slack',
+    license='MIT',
+    description='a DI container',
+    keywords='DI Container',
+    long_description=open('README.md').read(),
+    author='Feng Zhou',
+    author_email='zf.pascal@gmail.com',
+    packages=['slack'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
---
+++ tests/test_container.py
@@ -0,0 +1,85 @@
+from slack import Container, ParamterMissingError
+from pytest import raises
+
+
+class C:
+    pass
+
+
+class D:
+    def __init__(self, c):
+        self.c = c
+
+
+class E:
+    def __init__(self, d, c):
+        self.d = d
+        self.c = c
+
+
+def test_register():
+    c = Container()
+    c.register('c', C)
+    c1 = c.provide('c')
+    c2 = c.provide('c')
+    assert c1 is c2
+
+
+def test_reset():
+    c = Container()
+    c.register('c', C)
+    c1 = c.provide('c')
+    c.reset()
+    c2 = c.provide('c')
+    assert c1 is not c2
+
+
+def test_dependency():
+    c = Container()
+    c.register('d', D)
+    c.register('c', C)
+    assert c.provide('d').c is c.provide('c')
+
+
+def test_group_reset():
+    c = Container()
+    c.register('d', D)
+    c.register('c', C, group='test')
+    d1 = c.provide('d')
+    c1 = c.provide('c')
+    assert d1.c is c1
+
+    c.reset('test')
+    d2 = c.provide('d')
+    c2 = c.provide('c')
+    assert c2 is not c1
+    assert d2 is d1
+
+
+def test_partial_inject():
+    c = Container(d=D, c=C)
+    F = c.inject(E, 'd', 'c')
+    f1 = F()
+    assert f1.d.c is f1.c
+
+    F = c.inject(E, 'd')
+    f2 = F(c=C())
+    assert f2.d.c is not f2.c
+
+
+def test_apply():
+    c = Container(c=C, d=D, e=E)
+
+    def fn(c, d):
+        return c is d.c
+
+    assert c.apply(fn)
+    assert not c.apply(fn, c=C())
+
+    def fn(c, d, e, f):
+        return True
+
+    with raises(ParamterMissingError):
+        c.apply(fn)
+
+    assert c.apply(fn, f=None)
